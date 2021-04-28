d52ee1298d0740050ae72a7ed83b25e661c8b4f7
Amoras Tec <7902420-amorastec@users.noreply.gitlab.com>
2021-04-28 13:55:39 -0300

    refactor: eliminando importações não utilizadas

--- src/main/java/com/zup/apipoc/contacts/adapter/in/web/exceptions/RestResponseEntityExceptionHandler.java
+++ src/main/java/com/zup/apipoc/contacts/adapter/in/web/exceptions/RestResponseEntityExceptionHandler.java
@@ -8,10 +8,6 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 @ControllerAdvice
 public class RestResponseEntityExceptionHandler  extends ResponseEntityExceptionHandler {
 
