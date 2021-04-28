3302c183d6fa240a9f1a0e5e0b5686ee70cb03ec
Amoras Tec <7902420-amorastec@users.noreply.gitlab.com>
2021-04-28 14:57:57 -0300

    criando configuração inicial do open api - swagger 3

--- src/main/java/com/zup/apipoc/contacts/adapter/in/web/ContactController.java
+++ src/main/java/com/zup/apipoc/contacts/adapter/in/web/ContactController.java
@@ -6,6 +6,8 @@ import com.zup.apipoc.contacts.adapter.in.web.response.ContactResponse;
 import com.zup.apipoc.contacts.application.port.in.CreateContactCommand;
 import com.zup.apipoc.contacts.application.port.in.CreateContactUseCase;
 import com.zup.apipoc.contacts.application.port.in.ListContactsUseCase;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,12 +18,14 @@ import java.util.List;
 @RestController
 @RequestMapping("/contacts")
 @RequiredArgsConstructor
+@Tag(name = "Contact")
 public class ContactController {
 
     private final ContactResponseMapper contactResponseMapper;
     private final CreateContactUseCase createContactUseCase;
     private final ListContactsUseCase listContactsUseCase;
 
+    @Operation(description = "Endpoint to create new contact", summary = "Create new contact")
     @PostMapping
     public ResponseEntity<ContactResponse> createContact(@RequestBody final CreateContactRequest request) {
 
@@ -36,6 +40,7 @@ public class ContactController {
         return new ResponseEntity<>(contactResponse, HttpStatus.CREATED);
     }
 
+    @Operation(description = "Endpoint to list all contacts", summary = "List all contacts")
     @GetMapping
     public ResponseEntity<List<ContactResponse>> listContacts() {
 
