fc296b64801bf373d3065bbf5069bbbbdaf60627
Amoras Tec <7902420-amorastec@users.noreply.gitlab.com>
2021-04-28 13:19:22 -0300

    refactor: ajustando declaração atributo fieldErrors da classe invalidDomainException

--- src/main/java/com/zup/apipoc/commons/exceptions/InvalidDomainException.java
+++ src/main/java/com/zup/apipoc/commons/exceptions/InvalidDomainException.java
@@ -1,14 +1,12 @@
 package com.zup.apipoc.commons.exceptions;
 
 import lombok.Getter;
-import lombok.Setter;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class InvalidDomainException extends RuntimeException {
 
-    @Getter @Setter List<String> fieldErrors = new ArrayList<>();
+    @Getter private final List<String> fieldErrors;
 
     public InvalidDomainException(final String message, List<String> fieldErrors) {
         super(message);
