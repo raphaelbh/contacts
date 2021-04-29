8aecf3f57ff8c4275a2c5490124a3eda7d95b31c
Amoras Tec <7902420-amorastec@users.noreply.gitlab.com>
2021-04-29 00:33:21 -0300

    feat: inserindo informações do projeto no swagger

--- src/main/java/com/zup/apipoc/contacts/adapter/in/web/ContactController.java
+++ src/main/java/com/zup/apipoc/contacts/adapter/in/web/ContactController.java
@@ -7,6 +7,11 @@ import com.zup.apipoc.contacts.application.port.in.CreateContactCommand;
 import com.zup.apipoc.contacts.application.port.in.CreateContactUseCase;
 import com.zup.apipoc.contacts.application.port.in.ListContactsUseCase;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -15,17 +20,25 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/contacts")
-@RequiredArgsConstructor
-@Tag(name = "Contact")
+@Tag(name = "Contact", description = "gathers all endpoints referring to the contacts resource")
 public class ContactController {
 
     private final ContactResponseMapper contactResponseMapper;
     private final CreateContactUseCase createContactUseCase;
     private final ListContactsUseCase listContactsUseCase;
 
-    @Operation(description = "Endpoint to create new contact", summary = "Create new contact")
+
+
+    @Operation(description = "Endpoint to create new contact", summary = "successfully created contact")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "successfully created contact",
+            content = { @Content(mediaType = "application/json", schema = @Schema(implementation = ContactResponse.class))}),
+            @ApiResponse(responseCode = "400", description = "Request with invalid format", content = @Content),
+            @ApiResponse(responseCode = "500", description = "Internal server error", content = @Content)
+    })
     @PostMapping
     public ResponseEntity<ContactResponse> createContact(@RequestBody final CreateContactRequest request) {
 
@@ -40,7 +53,14 @@ public class ContactController {
         return new ResponseEntity<>(contactResponse, HttpStatus.CREATED);
     }
 
+
+
     @Operation(description = "Endpoint to list all contacts", summary = "List all contacts")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "list all contacts registered in the database",
+                    content = { @Content(mediaType = "application/json", schema = @Schema(implementation = ContactResponse.class))}),
+            @ApiResponse(responseCode = "500", description = "Internal server error", content = @Content)
+    })
     @GetMapping
     public ResponseEntity<List<ContactResponse>> listContacts() {
 
