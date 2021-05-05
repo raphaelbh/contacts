22c7c9dfd927bcbb909fe9d42ef8b60cd6b7c2e7
Amoras Tec <7902420-amorastec@users.noreply.gitlab.com>
2021-05-05 16:46:01 -0300

    feat: adicionando configuracao de consumidor do kafka

--- src/main/java/com/zup/contacts/contacts/adapter/in/consumer/kafka/ContactConsumer.java
+++ src/main/java/com/zup/contacts/contacts/adapter/in/consumer/kafka/ContactConsumer.java
@@ -1,6 +1,9 @@
 package com.zup.contacts.contacts.adapter.in.consumer.kafka;
 
+import com.zup.contacts.contacts.adapter.in.web.request.CreateContactRequest;
 import com.zup.contacts.contacts.adapter.out.persistence.dynamodb.ContactPersistenceDynamoDBAdapter;
+import com.zup.contacts.contacts.application.port.in.CreateContactCommand;
+import com.zup.contacts.contacts.application.port.in.CreateContactUseCase;
 import com.zup.contacts.contacts.domain.ContactDomain;
 import io.swagger.v3.oas.models.info.Contact;
 import lombok.RequiredArgsConstructor;
@@ -12,14 +15,19 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class ContactConsumer {
 
-    private final ContactPersistenceDynamoDBAdapter contactPersistenceDynamoDBAdapter;
-
-    private static final String TOPIC_CONTACT = "contacts";
+    private static final String TOPIC_CONTACT = "create-contact";
 
     private static final String GROUP_ID = "group_contact_id";
 
+    private final CreateContactUseCase createContactUseCase;
+
     @KafkaListener(topics = TOPIC_CONTACT, groupId = GROUP_ID, containerFactory = "kafkaListenerContainerFactory")
-    public void consumeContactToSave(ContactDomain contactDomain) {
-        contactPersistenceDynamoDBAdapter.save(contactDomain);
+    public void consumeContactToSave(CreateContactRequest createContactRequest) {
+        var command = CreateContactCommand.builder()
+                .name(createContactRequest.getName())
+                .phone(createContactRequest.getPhone())
+                .email(createContactRequest.getEmail())
+                .build();
+        createContactUseCase.execute(command);
     }
 }
--- src/main/java/com/zup/contacts/contacts/adapter/in/consumer/kafka/KafkaConsumerConfiguration.java
+++ src/main/java/com/zup/contacts/contacts/adapter/in/consumer/kafka/KafkaConsumerConfiguration.java
@@ -1,34 +1,35 @@
 package com.zup.contacts.contacts.adapter.in.consumer.kafka;
 
+import com.zup.contacts.contacts.adapter.in.web.request.CreateContactRequest;
 import com.zup.contacts.contacts.domain.ContactDomain;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.config.KafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
-import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
 import java.util.Map;
 
 
-@Component
+@Configuration
 public class KafkaConsumerConfiguration {
     @Bean
-    public ConsumerFactory<String, ContactDomain> consumerFactory(){
+    public ConsumerFactory<String, CreateContactRequest> consumerFactory(){
         Map<String,Object> config = new HashMap<>();
         config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,"localhost:9092");
         config.put(ConsumerConfig.GROUP_ID_CONFIG,"group_contact_id");
-        return new DefaultKafkaConsumerFactory<>(config, new StringDeserializer(), new JsonDeserializer<>(ContactDomain.class));
+        return new DefaultKafkaConsumerFactory<>(config, new StringDeserializer(), new JsonDeserializer<>(CreateContactRequest.class));
     }
 
     @Bean
-    KafkaListenerContainerFactory<ConcurrentMessageListenerContainer<String, ContactDomain>> kafkaListenerContainerFactory() {
-        ConcurrentKafkaListenerContainerFactory<String, ContactDomain> factory = new ConcurrentKafkaListenerContainerFactory<>();
+    KafkaListenerContainerFactory<ConcurrentMessageListenerContainer<String, CreateContactRequest>> kafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, CreateContactRequest> factory = new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(consumerFactory());
         return factory;
     }
